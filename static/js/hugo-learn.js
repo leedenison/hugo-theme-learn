67413e1526e19d7b30b4fb9f03c6415fe81fd892
Lee Denison <lee@longlost.info>
2018-08-04 10:34:27 -0700

    Customized the Grav theme for local codelab documentation.

--- static/js/hugo-learn.js
+++ static/js/hugo-learn.js
@@ -21,9 +21,7 @@ var images = $("div#body-inner img").not(".inline");
 // Wrap image inside a featherlight (to get a full size view in a popup)
 images.wrap(function(){
   var image =$(this);
-  if (!image.parent("a").length) {
-    return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
-  }
+  return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
 });
 
 // Change styles, depending on parameters set to the image
